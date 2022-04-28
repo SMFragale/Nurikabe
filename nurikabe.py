d640d82b787287019c9b8f52cb10742ce9be6aad
Sebastian <smolanof@javeriana.edu.co>
2022-04-28 17:49:25 -0500

    Se cambio el approach para ganar el tablero. Se elimino el tipo NO_ASIGNADO y mas bien ahora el tablero inicia lleno de casillas tipo ISLA y debe llenarse en cada turno el MAR

--- nurikabe.py
+++ nurikabe.py
@@ -20,13 +20,11 @@ class Nurikabe:
 
     MAR = "M"
     ISLA = "I"
-    NO_ASIGNADO = "-"
 
     def __init__(self, n: int, m: int) -> None:
         self.n = n
         self.m = m
-        self.tablero = [[self.NO_ASIGNADO for i in range(m)] for j in range(n)]
-        print(self.tablero)
+        self.tablero = [[self.ISLA for i in range(m)] for j in range(n)]
 
     # Imprime el tablero
     def imprimirTablero(self):
@@ -37,7 +35,7 @@ class Nurikabe:
     
     # Pinta la celda en la coordenada x, y.
     #Retorna False si el juego continua, True si el juego se acabo
-    def pintarCelda(self, x: int, y: int, tipo: str) -> bool:
+    def pintarCelda(self, x: int, y: int) -> bool:
         if x > self.n or y > self.m or x < 0 or y < 0:
             print(f"Las coordenadas deben estar en el rango del tablero. x: {self.n}, y: {self.m}")
         
@@ -45,15 +43,8 @@ class Nurikabe:
         for fila in self.tablero:
             copiaTablero.append([j for j in fila])
 
-        copiaTablero[x][y] = tipo
+        copiaTablero[x][y] = self.MAR
         #Realizar jugada fantasma
-        
-        #Condiciones
-        #Cada celda numerada es una celda de isla, el número en ella es el número de celdas en esa isla.
-        #Cada isla debe contener solamente una celda numerada.
-        
-        #self.numCeldasIsla(copiaTablero)
-        #self.islaUnica(copiaTablero)
 
         #Debe haber un solo mar, que no puede contener "piscinas", es decir, áreas de 2X2 de celdas negras.
         if not self.marUnico(x, y, copiaTablero):
@@ -63,27 +54,17 @@ class Nurikabe:
             print("Con esta jugada se genera una piscina")
             return False
         
-        if not self.revisarIslas(copiaTablero):
-            print("Con esta jugada existe una isla incorrecta")
-            return False
+        #if not self.revisarIslas(copiaTablero):
+            #print("Con esta jugada existe una isla incorrecta")
+            #return False
         
-        self.tablero[x][y] = tipo
+        self.tablero[x][y] = self.MAR
 
-        if self.tableroLleno():
-            if self.verificarTableroLleno():
-                print("Victoria! El tablero fue completado correctamente")
-            else:
-                print("El tablero no es una solucion valida")
-        else:
-            return False
+        if self.verificarTableroLleno():
+            print("Victoria! El tablero fue completado correctamente")
+            return True
+        return False
 
-    #Indica si el tablero esta lleno
-    def tableroLleno(self):
-        for i in range(len(self.tablero)):
-            for j in range(len(self.tablero[i])):
-                if self.tablero[i][j] == self.NO_ASIGNADO:
-                    return False
-        return True
     
     #Solo debe llamarse cuando el tablero esta lleno. Indica si el jugador gana o pierde
     def verificarTableroLleno(self):
@@ -126,26 +107,6 @@ class Nurikabe:
         self.marcarMar(x-1, y, tablero, marcados)
         self.marcarMar(x, y+1, tablero, marcados)
         self.marcarMar(x, y-1, tablero, marcados)
-
-    #Revisa todas las islas del tablero pasado por parametro para ver que cumplan con la condicion suave
-    def revisarIslas(self, tablero):
-        for i in range(len(tablero)):
-            for j in range(len(tablero[i])):
-                if type(tablero[i][j]) == int :
-                    if not self.islaValida(i, j, tablero):
-                        return False
-        return True
-
-    #Indica si una isla es valida durante el juego verificando la condicion: El numero en la celda numerada de una isla indica cuantas celdas tiene dicha isla
-    #Ya que las islas se van poniendo casilla por casilla, en este metodo solo se verifica que la isla tenga una cantidad de casillas menor o igual a la cantidad que requiere 
-    def islaValida(self, x, y, tablero):
-        if type(tablero[x][y]) !=  int:
-            return False
-        marcados = {}
-        casillasIsla = self.contarCasillasIsla(x, y, tablero, marcados)
-        if(casillasIsla > tablero[x][y]):
-            return False
-        return True
     
     #Indica si la isla es valida a nivel general (solo debe llamarse cuando el tablero este lleno)
     def islaValidaFinal(self, x, y):
@@ -188,25 +149,25 @@ def leerTablero(archivo: str) -> Nurikabe:
 
     return nkb
 
+clear()
 nkb: Nurikabe = leerTablero("tablero1.nkb")
 juegoTerminado = False
 while not juegoTerminado:
     nkb.imprimirTablero()
-    print("Indique las coordenadas de una casilla para pintar siguiendo este formato:\n x,y,T") 
-    print("Donde 'x' corresponde a fila, 'y' a la columna y T a un tipo de casilla que se quiere pintar: 'M' para pintar MAR, 'I' para pintar ISLA")
-    print("Ejemplo: 1,1,M")
-    print("Nota: las filas y columnas empiezan desde el indice 0, por lo tanto para un tablero de 5x5 no es valido ingresar algo como: 1,5,M")
+    print("Indique las coordenadas de una casilla para pintar el MAR siguiendo este formato:\n x,y") 
+    print("Donde 'x' corresponde a fila, 'y' a la columna")
+    print("Ejemplo: 1,1")
+    print("Nota: las filas y columnas empiezan desde el indice 0, por lo tanto para un tablero de 5x5 no es valido ingresar algo como: 1,5")
     entrada = input()
     entrada = entrada.split(",")
     try:
         x = int(entrada[0])
         y = int(entrada[1])
-        Val = entrada[2]
-        if Val != "M" and Val != "I":
-            raise Exception()
     except:
         print("Error en las entradas")
-    clear()
-    juegoTerminado = nkb.pintarCelda(x, y, Val)
+        continue
+    finally:
+        clear()
+    juegoTerminado = nkb.pintarCelda(x, y)
     print()
     
